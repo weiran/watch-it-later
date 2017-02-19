de1ed5204173526201832b01205823a8e00c20e9
Weiran Zhang <weiran@zhang.me.uk>
2017-02-19 14:03:39 +0000

    Removed unnecessary imports in bridging header

--- App/Supporting Files/WatchItLater-Bridging-Header.h
+++ App/Supporting Files/WatchItLater-Bridging-Header.h
@@ -2,10 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-
-#import <XCDYouTubeKit/XCDYouTubeKit.h>
-#import <YTVimeoExtractor/YTVimeoExtractor.h>
-#import <AsyncImageView/AsyncImageView.h>
-#import <SVProgressHUD/SVProgressHUD.h>
-
 #import "InstapaperKit.h"
