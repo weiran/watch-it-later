e442d6f515cd8525eeb6229034afdeec18a5ab0b
Weiran Zhang <weiran@zhang.me.uk>
2021-10-17 11:21:23 +0100

    Add SwinjectStoryboard init
    Upgrading required adding this line in for it to work

--- App/AppDelegate.swift
+++ App/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftyUserDefaults
+import SwinjectStoryboard
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         _ = Database.shared
+        SwinjectStoryboard.configure()
         return true
     }
 
